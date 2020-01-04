28fa5213838f64ed663ee15dcf7b9bf92db109b5
EquiFox <contact@reversing.ninja>
2020-01-04 14:22:39 -0500

    Removed unused file

--- ProjectWinterHack/functions.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "classes.hpp"
-
-typedef PlayerHandler* __fastcall GameManager__GetPlayerHandler(GameManager* gameManager, int playerId, bool includeGhost);
-typedef void __fastcall TextMeshProUGUI__SetFaceColor(DWORD64* textMeshProUGUI, Color32 newColor);
-typedef void __fastcall TextMeshProUGUI__SetAllDirty(DWORD64* textMeshProUGUI);
-typedef void ObscuredCheatingDetector__StopDetection();
